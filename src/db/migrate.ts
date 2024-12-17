c401c866b9c0dc3f2805689fdfca045ab44ec163
HeitorNOC <heiitornoc@gmail.com>
2024-12-17 17:12:13 -0300

    fix

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,5 +1,5 @@
-import postgres from 'postgres'
 import chalk from 'chalk'
+import postgres from 'postgres'
 
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
@@ -7,6 +7,7 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import { env } from '@/env'
 
 const connection = postgres(env.DB_URL, { max: 1 })
+console.log(`connection: ${env.DB_URL}`)
 const db = drizzle(connection)
 
 await migrate(db, { migrationsFolder: 'drizzle' })
